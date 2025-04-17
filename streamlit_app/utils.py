53e44080501fc69f22f1c1bd9ad3bafe97237307
Vivek Malladi <vivek@MacBookAir.bbrouter>
2025-04-17 08:56:11 +0530

    Add prod backend service url

--- streamlit_app/utils.py
+++ streamlit_app/utils.py
@@ -1,7 +1,7 @@
 import requests
 
 def call_hiregenius_api(file_path, job_description):
-    url = "http://127.0.0.1:8000/upload-resume/"
+    url = "https://genie-5hgp.onrender.com/upload-resume/"
     with open(file_path, "rb") as f:
         files = {"file": f}
         data = {"job_description": job_description}
